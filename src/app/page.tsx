0343c0643e94f6dc70d176ff0e0a4bacdcc50f95
zidanalif <zidan@solusiciptamedia.com>
2025-06-30 09:07:35 +0700

    Add product API and update Home page to display products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-"use client";
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
 
-export default function Home() {
-  return <div className="site-content">Home Page</div>;
+async function getProduct(): Promise<Product[]> {
+  const res = await fetch("https://localhost:3000/api/products");
+  return res.json() as Promise<Product[]>;
+}
+
+export default async function Home() {
+  const products = await getProduct();
+
+  return (
+    <>
+      {products.map((product) => (
+        <div key={product.id} className="p-4 border rounded shadow-md">
+          <img
+            src={product.imageUrl}
+            alt={product.name}
+            className="w-full h-48 object-cover mb-4"
+          />
+          <h2 className="text-xl font-bold mb-2">{product.name}</h2>
+          <p className="text-gray-700 mb-2">{product.description}</p>
+          <p className="text-lg font-semibold">
+            Rp {product.price.toLocaleString()}
+          </p>
+        </div>
+      ))}
+    </>
+  );
 }
